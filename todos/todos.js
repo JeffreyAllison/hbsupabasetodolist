443f4b1932c3fc47c9b731463ca8f48c9020191e
JeffAllison <itsjallison@gmail.com>
2022-03-28 12:42:15 -0700

    added async to necessary functions in fetch.js for signup/in functions

--- todos/todos.js
+++ todos/todos.js
@@ -1,10 +1,10 @@
-import { 
-    checkAuth, 
-    createTodo, 
-    completeTodo,
-    getTodos,
-    logout,
-    deleteAllTodos, 
+import {
+  checkAuth,
+  createTodo,
+  completeTodo,
+  getTodos,
+  logout,
+  deleteAllTodos,
 } from '../fetch-utils.js';
 import { renderTodo } from '../render-utils.js';
 
@@ -15,29 +15,29 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
-todoForm.addEventListener('submit', async(e) => {
-    // on submit, create a todo, reset the form, and display the todos
+todoForm.addEventListener('submit', async (e) => {
+  // on submit, create a todo, reset the form, and display the todos
 });
 
-async function displayTodos() {
-    // fetch the todos
-    
-    // display the list of todos
+async function displayTodos () {
+  // fetch the todos
 
-    // be sure to give each todo an event listener
+  // display the list of todos
 
-    // on click, complete that todo
+  // be sure to give each todo an event listener
+
+  // on click, complete that todo
 }
 
 // add an on load listener that fetches and displays todos on load
 
 logoutButton.addEventListener('click', () => {
-    logout();
+  logout();
 });
 
 
-deleteButton.addEventListener('click', async() => {
-    // delete all todos
+deleteButton.addEventListener('click', async () => {
+  // delete all todos
 
-    // then refetch and display the updated list of todos
+  // then refetch and display the updated list of todos
 });
