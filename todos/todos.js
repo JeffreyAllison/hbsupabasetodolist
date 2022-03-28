a84ba66489835292e1b3f0a2ee998168e3ed2890
JeffAllison <itsjallison@gmail.com>
2022-03-28 14:43:34 -0700

    completed displayTodos function

--- todos/todos.js
+++ todos/todos.js
@@ -18,12 +18,32 @@ const deleteButton = document.querySelector('.delete-button');
 todoForm.addEventListener('submit', async (e) => {
   // on submit, create a todo, reset the form, and display the todos
   e.preventDefault();
+
+  const data = new FormData(form);
+
+  await createTodoItem({
+    todo: data.get('todo'),
+    completed: false,
+  });
+
+  await displayTodos();
+
 });
 
 async function displayTodos () {
   // fetch the todos
+  todosEl.textContent = '';
 
   // display the list of todos
+  const todoList = await getTodos();
+
+  for (let todoItem of Todos) {
+    const todoItemEl = document.createElement('p');
+
+    todoItemEl.textContent = `${todo.todo}`;
+
+    todosEl.append(todoItemEl);
+  }
 
   // be sure to give each todo an event listener
 
